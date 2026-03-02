270536f1b50cc6de1fb3ef90516e92a4fa7d62fd
Zihan Gao <cgao@lcs.on.ca>
2026-03-02 10:57:58 -0500

    Initial Commit

---
+++ MyBigApp/MyBigAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBigAppApp.swift
+//  MyBigApp
+//
+//  Created by GaoZiHan on 2026/3/2.
+//
+
+import SwiftUI
+
+@main
+struct MyBigAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
